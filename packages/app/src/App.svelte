b7d3ba2c7001be660002fd0e7a572a5814b79051
Evgenij Beloded <belodedevgenij@gmail.com>
2021-10-30 09:20:13 +0700

    Bootstrap client app

---
+++ packages/app/src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts" context="module">
+const hasEthereum = !!ethereum
+</script>
+
+<script lang="ts">
+async function connect() {}
+</script>
+
+hasEthereum : {hasEthereum}
+
+{#if hasEthereum}
+  <button on:click="{connect}">Connect</button>
+{/if}
---
+++ packages/app/src/vite-env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="svelte" />
+/// <reference types="vite/client" />
+
+declare let ethereum: import('eip1193-provider').default
