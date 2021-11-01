e89303024c5ca863667cef89633a29f39b79ab41
Evgenij Beloded <belodedevgenij@gmail.com>
2021-11-01 10:14:25 +0700

    Create Cryptogift.sol

---
+++ packages/ethereum/src/Cryptogift.sol
@@ -0,0 +1,44 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import 'hardhat/console.sol';
+
+enum GiftType {
+  Eth,
+  ERC20,
+  ERC721
+}
+
+contract Cryptogift {
+  mapping(string => uint256) public giftMap;
+
+  /// @notice Calculate tree age in years, rounded up, for live trees
+  /// @dev The Alexandr N. Tetearing algorithm could increase precision
+  function stashETH(string memory key, uint256 amount) public payable {
+    console.log('key', key);
+    console.log('amount', amount);
+    console.log('value', msg.value);
+    console.log('sender', msg.sender);
+    if (giftMap[key] != 0) {
+      revert('Key already exists');
+    }
+
+    giftMap[key] = amount;
+  }
+
+  function get(string memory key) public view returns (uint256) {
+    console.log('key', key);
+    return giftMap[key];
+  }
+
+  function unstash(
+    address recipient,
+    string memory key,
+    GiftType kind
+  ) public {
+    console.log('key', key);
+    // console.log('kind', kind);
+    console.log('recipient', recipient);
+    console.log('sender', msg.sender);
+  }
+}
