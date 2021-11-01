74d64b11c90fe59aa55fb289cb9b178f1c640197
Evgenij Beloded <belodedevgenij@gmail.com>
2021-11-01 10:41:29 +0700

    Refactor to remove hard-coded values

--- packages/app/src/routes/index.svelte
+++ packages/app/src/routes/index.svelte
@@ -2,11 +2,42 @@
 import { onMount } from 'svelte'
 
 let balance: any = 0
-onMount(() => {
+let key = 'unique-key'
+let amount: number = 0
+
+function updateBalance() {
   import('@cryptogifts/ethereum')
     .then(({ getBalance }) => getBalance())
     .then((v) => (balance = v))
+}
+
+async function stash() {
+  const { contract } = await import('@cryptogifts/ethereum')
+  try {
+    await contract(true).stashETH(key, amount, { value: amount })
+  } catch (e) {
+    console.log(e.error)
+  }
+}
+
+async function get() {
+  const { contract } = await import('@cryptogifts/ethereum')
+
+  const stashed = await contract().get(key)
+  console.log({ stashed: stashed.toNumber() })
+}
+
+onMount(() => {
+  updateBalance()
 })
 </script>
 
-{balance}
+Balance: {balance}
+
+<input placeholder="key" type="text" bind:value="{key}" />
+<input type="number" bind:value="{amount}" />
+<button on:click="{stash}">Stash</button>
+
+<hr />
+
+<button on:click="{get}">Get</button>
--- packages/ethereum/index.ts
+++ packages/ethereum/index.ts
@@ -5,25 +5,21 @@ declare global {
 }
 
 import { ethers } from 'ethers'
-import { Payable__factory } from './contracts'
+import { Cryptogift__factory } from './contracts'
+import addresses from './contracts/addresses.json'
+import accounts from './hardhat-accounts.json'
 
-const PAYABLE_ADDRESS = '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0'
-
+const ADDRESS = addresses.localhost.Cryptogift
 const provider = () => new ethers.providers.JsonRpcProvider()
 const signer = () => {
-  const wallet = new ethers.Wallet(
-    '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
-    provider(),
-  )
-  console.log({ wallet })
-  return wallet
+  return new ethers.Wallet(accounts[0].private_key, provider())
 }
 
 export const contract = (sign: boolean = false) => {
   const s = sign ? signer() : provider()
-  return Payable__factory.connect(PAYABLE_ADDRESS, s)
+  return Cryptogift__factory.connect(ADDRESS, s)
 }
 
 export const getBalance = async () => {
-  return provider().getBalance(PAYABLE_ADDRESS)
+  return provider().getBalance(ADDRESS)
 }
