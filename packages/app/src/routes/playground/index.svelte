9f98b079622dbcf4e7d3b45b760006b74a839468
Evgenij Beloded <belodedevgenij@gmail.com>
2021-11-20 06:45:26 +0700

    Update index.svelte

--- packages/app/src/routes/playground/index.svelte
+++ packages/app/src/routes/playground/index.svelte
@@ -2,11 +2,7 @@
 import { onMount } from 'svelte'
 import { api } from '$lib/services/api'
 
-onMount(async () => {
-  const result = await api.hello()
-
-  console.log({ result })
-})
+onMount(async () => {})
 </script>
 
 <slot />
