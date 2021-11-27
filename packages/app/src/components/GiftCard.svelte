a4da9291c5928ca9a98bb9a9b993f9714677d719
Evgenij Beloded <belodedevgenij@gmail.com>
2021-11-27 21:55:26 +0700

    Implement gifts list

--- packages/app/src/components/GiftCard.svelte
+++ packages/app/src/components/GiftCard.svelte
@@ -1,15 +1,25 @@
 <!-- shows information about the gift -->
 <script lang="ts">
-import type { GiftStruct } from '$lib/services/ethereum'
-
-import type { RedeemableGift } from '$lib/types'
+import { GiftStruct, utils } from '$lib/services/ethereum'
 
 export let gift: GiftStruct
+$: console.log({ gift })
+const STATUSES = ['None', 'Pending', 'Redeemed']
+const GIFT_TYPES = ['ETH', 'ERC20', 'ERC721']
 </script>
 
-Gift Card Start
-<pre>
-{JSON.stringify(gift, null, 2)}
-</pre>
-
-Gift Card End
+<div
+  class="bg-base-100 rounded-xl border-1 border-primary relative"
+  style="padding-top: 61.8%"
+>
+  <div class="absolute inset-4 font-mono text-sm">
+    <div>Type: {GIFT_TYPES[Number(gift.giftType)]}</div>
+    <div class="absolute bottom-0 left-0">
+      <div>Value: {utils.formatEther(gift.giftValue)} ETH</div>
+      <div>
+        For Fees: {utils.formatUnits(gift.giftGas, 'gwei')} gwei
+      </div>
+    </div>
+    <div class="absolute top-0 right-0">{STATUSES[Number(gift.status)]}</div>
+  </div>
+</div>
--- packages/app/src/routes/gifts/index.svelte
+++ packages/app/src/routes/gifts/index.svelte
@@ -1,24 +1,22 @@
 <script lang="ts">
-import { contract$, user$ } from '$lib/services/ethereum'
+import { contract$ } from '$lib/services/ethereum'
 import { GiftCard } from '$components'
-
-$: console.log({ contract: $contract$ })
-$: console.log({ user: $user$ })
 </script>
 
-{#if $contract$}
-  {#await $contract$.getMyGifts()}
-    <!-- $contract$. is pending -->
-    loading
-  {:then gifts}
-    <!-- $contract$. was fulfilled -->
-    {#each gifts as gift}
-      <GiftCard gift={gift} />
-    {/each}
-  {:catch error}
-    <pre>{JSON.stringify(error, null, 2)}</pre>
-    <!-- $contract$. was rejected -->
-  {/await}
-{/if}
-
-List my gifts - available only when a wallet is connected
+<div class="min-h-screen p-20 mx-auto">
+  {#if $contract$}
+    {#await $contract$.getMyGifts()}
+      <!-- $contract$. is pending -->
+      loading
+    {:then gifts}
+      <!-- $contract$. was fulfilled -->
+      <div class="grid grid-cols-3 gap-4">
+        {#each gifts as gift}
+          <div>
+            <GiftCard gift={gift} />
+          </div>
+        {/each}
+      </div>
+    {/await}
+  {/if}
+</div>
